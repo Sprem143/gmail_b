68fe8182bb3630c941a812b6884ace489c20bc6c
Prem Kumar <prem68265@gmail.com>
2025-06-19 12:12:59 +0530

    db

--- controllers/sender.js
+++ controllers/sender.js
@@ -49,7 +49,6 @@ exports.deletesenderemail = async (req, res) => {
 exports.editsenderemail = async (req, res) => {
     try {
         const {editid, email, appPassword } = req.body;
-        console.log(editid, email, appPassword)
         await Sender.findOneAndUpdate(
             { _id: editid },
             {$set:{email:email, appPassword:appPassword}}
