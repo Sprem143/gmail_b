2d4013e46c2ad6390fab1514c30f00dda5872b1f
Prem Kumar <prem68265@gmail.com>
2025-08-04 22:49:52 +0545

    Update emailRoutes.js

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {saveSender,getSenders, deletesenderemail, editsenderemail} = require('../controllers/sender');
+const { signin, signup, dashboard} = require('../controllers/auth')
 const {sendEmails} = require('../controllers/receiver')
 
 router.post('/savesender', saveSender);
@@ -8,5 +9,8 @@ router.get('/getsender', getSenders);
 router.post('/sendemails', sendEmails);
 router.post('/deletesenderemail', deletesenderemail);
 router.post('/editsenderemail', editsenderemail);
+router.post('/api/signin', signin)
+router.post('/api/signup', signup)
+router.post('/verify-token', dashboard)
 
 module.exports = router;
